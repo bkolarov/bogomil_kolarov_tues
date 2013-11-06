0459bd715530857cdc1736cc13e33c6afa6a91c1
Crash <crash@crash-ID.(none)>
2013-11-06 21:33:50 +0200

    Task 3 is done

--- Belt_1.2_Prepare/belt_3.rb
+++ Belt_1.2_Prepare/belt_3.rb
@@ -1,17 +1,21 @@
 require 'csv'
 
 file_name = ARGV[0]
+parameter = ARGV[1]
+arr = []
+
+CSV.foreach(file_name, "r") do |row|
+	if row[2].to_f > parameter.to_f
+		arr << row
+	end
+end
 
 
+arr = arr.sort_by{|x| x[2].to_i}
+arr = arr.reverse()
+file_name = file_name.split('.csv')
 
-
-
-
-
-
-
-
-CSV.open("#{file_name[0]}_result2.csv", "wb") do |f|
+CSV.open("#{file_name[0]}_result3.csv", "wb") do |f|
 	arr.each do |x|
 		f << x
 	end
