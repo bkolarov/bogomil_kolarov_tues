7925bddb7ce216e52ea08b7b34e78b74a6ae7681
Bogomil <Crash-ID@users.noreply.github.com>
2014-01-22 22:43:25 +0200

    Update Task2.rb

--- Yellow1_Tasks/Task2/Task2.rb
+++ Yellow1_Tasks/Task2/Task2.rb
@@ -18,3 +18,24 @@ CSV.open(outputFileName, "wb") do |row|
 		row << [key, value]
 	end
 end
+
+require 'csv'
+
+fileName = ARGV[0]
+outputFileName = fileName.gsub(".csv", "") + "_result.csv"
+
+hash = Hash.new {|hash,k| hash[k] = [[],0]}
+
+CSV.foreach(fileName) do |row|
+	valute = row[4].split(" ")[0]
+	#sum = hash[valute][1].to_i + row[2].to_i
+	hash[valute] = valute, hash[valute][1].to_i + row[2].to_i
+end
+
+hash = hash.sort_by {|k,v| v[1].to_i}
+hash.each {|k,v| puts v}
+CSV.open(outputFileName, "wb") do |row|
+	hash.each do |key,value|
+		row << value
+	end
+end
